fec9b1e6d3105ac9184673835c2466824eb12bff
CTeillet <teillet.corentin@neuf.fr>
2020-05-07 23:40:08 +0200

    verification du texte de la cellule dans la question et non de expr

--- partie2/src/exceml.ml
+++ partie2/src/exceml.ml
@@ -50,7 +50,7 @@ let propagate grid infos_grid i j = assert false (* TODO *)
 
 let grid_to_string grid infos_grid = (* Question 4 *)
   let res = ref [] in
-  Array.iteri (fun i e -> Array.iteri (fun j f -> if grid.(i).(j) != Tableur.Vide then res := (Printf.sprintf "%d|%d|%s" i j  (Dom.Input.get_value f.inp))::!res) e) infos_grid;
+  Array.iteri (fun i e -> Array.iteri (fun j f -> if Dom.Input.get_value (infos_grid.(i).(j).inp) != "" then res := (Printf.sprintf "%d|%d|%s" i j  (Dom.Input.get_value f.inp))::!res) e) infos_grid;
   String.concat "\n" !res
 
 let cells_of_string storage_grid = assert false (* TODO *)
